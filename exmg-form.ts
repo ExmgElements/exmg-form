e297d0128d316b589dcec6c69e0fe72feb186cca
uladzislau <uladzislau.zayats@amsterdam-standard.pl>
2019-02-15 17:22:08 +0100

    PTWO-283 As a developer we need a standardized way of impl forms for the cms

--- exmg-form.ts
+++ exmg-form.ts
@@ -6,13 +6,59 @@ import '@polymer/paper-icon-button';
 
 @customElement('exmg-form')
 export class ExmgForm extends LitElement {
+  @query('#iron-form')
+  private ironFormElem?: HTMLElement|any;
+
+  constructor() {
+    super();
+  }
+
+  private onIronFormSubmit(event: CustomEvent) {
+    this.dispatchEvent(new CustomEvent('submit', {bubbles: false, composed: true, detail: event.detail}));
+  }
+
+  private onSubmitBtnClick() {
+    console.log('onSubmitBtnClick');
+    this.ironFormElem!.submit();
+  }
+
   protected render() {
     return html`
-      <iron-form>
+      <!--suppress CssUnresolvedCustomProperty -->
+      <style>
+        paper-button.primary {
+          background: var(--primary-color);
+          color: white;
+        }
+        paper-button {
+          border-radius: 6px;
+          text-transform: initial;
+          padding-left: 16px;
+          padding-right: 16px;
+          -webkit-border-radius: 8px;
+          border-radius: 8px;
+          text-transform: none;
+          letter-spacing: .25px;
+          min-width: 60px;
+        }
+        paper-button[disabled] {
+          opacity: 0.5;
+        }
+        paper-button.error,
+        paper-button.alert {
+          color: white;
+          background: var(--error-color);
+        }
+      </style>
+      <iron-form id="iron-form" @iron-form-submit="${this.onIronFormSubmit}">
         <form>
           <slot></slot>
+          <div class="actions">
+            <paper-button dialog-dismiss>Cancel</paper-button>
+            <paper-button @click="${this.onSubmitBtnClick}" class="primary">Submit</paper-button>
+          </div>
         </form>
       </iron-form>
-`;
+    `;
   }
 }
